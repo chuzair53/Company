f8eaf33b0bb8a0a9271f4c9bc2cf8aa54b00dd1b
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-07-13 17:57:41 +0500

    added bootstrap cdn

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -7,6 +7,7 @@
     @yield('header-meta')
 	<title>@yield('title')</title>
     @yield('style')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{url('css/app.css')}}">
 </head>
 <body>
