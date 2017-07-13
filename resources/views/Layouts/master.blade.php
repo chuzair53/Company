8c308fdf9a1aa0d7f9ed9ad76eac45b21882fe09
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-07-13 16:52:15 +0500

    FIXED EVERYTHING

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -5,23 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     @yield('header-meta')
-	  <title>@yield('title')</title>
-	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>@yield('title')</title>
     @yield('style')
-    <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
-	  <script src="https://use.fontawesome.com/0ee43d495e.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{url('css/app.css')}}">
 </head>
 <body>
 @include('partials.header')
 
-    
       @yield('content')
 
-    
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"
-  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-  crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="{{url('js/style.js')}}"></script>
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/0ee43d495e.js"></script>
+<script src="{{url('js/app.js')}}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-const { mix } = require('laravel-mix');
+const {
+    mix
+} = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +13,8 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/scripts/app.js', 'public/js')
+    .sass('resources/assets/styles/app.scss', 'public/css')
+    .browserSync({
+        proxy: 'localhost:8000'
+    });
