17ab9a4dda89988e5b992a19acef7fc439442d36
Chuzair53 <chuzair53@gmail.com>
2017-07-13 08:32:25 -0700

    Upload

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -12,9 +12,9 @@
                 </div>
                 <div id="navbar" class="collapse navbar-collapse navbar-right">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li class="active"><a href="#">HOME</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
                     </ul>
                 </div>
             </div>
